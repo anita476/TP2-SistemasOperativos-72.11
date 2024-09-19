7ba784c6c3ceb3bda6ca7b12dc062269dfedebb1
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-09-18 21:58:57 -0300

    added the base code viewed in class (not compilable)

---
+++ Kernel/memoryManagement.c
@@ -0,0 +1,28 @@
+#define BLOCK_SIZE 10
+#define BLOCK_COUNT 10
+void * start, current;
+int size;
+void * free_ptrs[BLOCK_COUNT];
+int current = 0;
+void my_mm_init(void *p, int s){
+	start = p;
+	size = s;
+	
+	free_ptrs[0] = start;
+	free_ptrs[1] = start + BLOCK_SIZE ; //y etc.. meter en un for
+	free_ptrs[2] = start + BLOCK_SIZE + BLOCK_SIZE;
+	current = 0;
+
+}
+
+void * my_malloc(){
+	if(current <  size + start){
+		return free_ptrs[current++];
+	}
+	return NULL
+}
+
+void my_free(void * p){
+	free_ptrs[--current] = p;
+
+}

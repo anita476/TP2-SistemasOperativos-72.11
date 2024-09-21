ceb165bff03ea9ead9aaf1d68bb6c57270a16329
Ana Negre <98236062+anita476@users.noreply.github.com>
2024-09-21 14:41:19 -0300

    added some prints for clarity

--- Kernel/test_mm.c
+++ Kernel/test_mm.c
@@ -12,6 +12,7 @@ typedef struct MM_rq {
 } mm_rq;
 
 uint64_t test_mm(uint64_t argc, char *argv[]) {
+  print("Starting test...\n");
 
   mm_rq mm_rqs[MAX_BLOCKS];
   uint8_t rq;
@@ -31,11 +32,13 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
     total = 0;
 
     // Request as many blocks as we can
+    print("Requesting blocks...\n");
     while (rq < MAX_BLOCKS && total < max_memory) {
       mm_rqs[rq].size = GetUniform(max_memory - total - 1) + 1;
       mm_rqs[rq].address = malloc(mm_rqs[rq].size);
 
       if (mm_rqs[rq].address) {
+        print("Succesful malloc\n");
         total += mm_rqs[rq].size;
         rq++;
       }
@@ -45,6 +48,7 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
     uint32_t i;
     for (i = 0; i < rq; i++){
       if (mm_rqs[i].address){
+        print("Memset passed\n");
         memset(mm_rqs[i].address, i, mm_rqs[i].size);
       }
     }
@@ -56,15 +60,19 @@ uint64_t test_mm(uint64_t argc, char *argv[]) {
           print("test_mm ERROR\n");
           return -1;
         }
+        else{
+          print("Memcheck passed\n");
+        }
       }
     }
 
     // Free
     for (i = 0; i < rq; i++){
       if (mm_rqs[i].address){
+        print("Freeing block\n");
         free(mm_rqs[i].address);
       }
     }
+    print("Iteration done\n");
   }
-
 }

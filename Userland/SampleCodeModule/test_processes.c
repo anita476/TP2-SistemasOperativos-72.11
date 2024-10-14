391b725733e12cdfa91edb3dd8fa823269d5243a
Matías Leporini <matilepo03@gmail.com>
2024-10-14 15:21:24 -0300

    PVS

--- Userland/SampleCodeModule/test_processes.c
+++ Userland/SampleCodeModule/test_processes.c
@@ -1,9 +1,10 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 #include "test_util.h"
 #include <defs.h>
 #include <commands.h>
-#include <libSysCalls.h>
-
-             
+#include <libSysCalls.h>   
 
 typedef struct P_rq {
   int32_t pid;
--- Userland/SampleCodeModule/test_util.c
+++ Userland/SampleCodeModule/test_util.c
@@ -1,10 +1,9 @@
 // This is a personal academic project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
-#include <stdint.h>
-
 #include <libSysCalls.h>
 #include <commands.h>
+#include <stdint.h>
 
 // Random
 static uint32_t m_z = 362436069;
